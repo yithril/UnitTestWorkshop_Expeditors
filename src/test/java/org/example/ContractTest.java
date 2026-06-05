8f5534a2ca951b21991e7d6dfc11388ed5f6ab5c
yithril <yithril@gmail.com>
2026-06-04 23:50:07 -0400

    verbiage change

--- src/test/java/org/example/ContractTest.java
+++ src/test/java/org/example/ContractTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 class ContractTest {
 
     @Test
-    void writeTestsFromLabMd() {
-        // TODO: Write SalesContract and LeaseContract tests using the rules in LAB.md.
+    void writeTests() {
+        // TODO: Write SalesContract and LeaseContract tests
         // Contract amounts use BigDecimal. Compare with:
         //   assertEquals(0, expected.compareTo(actual));
         // or assertEquals(expected, actual) when both use the same scale (2 decimal places).
